8e968e22432657c530fa4cc58cba23ee39d5e700
Jagathis10 <122803221+Jagathis10@users.noreply.github.com>
2024-05-20 13:14:44 +0530

    Add files via upload

---
+++ t_cell_database.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `slimedatabase`
+ */
+
+/* `slimedatabase`.`t cell database` */
+$t_cell_database = array(
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '33','end' => '41','Length' => '8','Peptide' => 'EPRTLAAAK'),
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '69','end' => '81','Length' => '12','Peptide' => 'RPSSCARLRHVAR'),
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '84','end' => '92','Length' => '8','Peptide' => 'ADAQAVLSG'),
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '127','end' => '133','Length' => '6','Peptide' => 'SSRKVPG'),
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '44','end' => '51','Length' => '7','Peptide' => 'NFYKIPDN'),
+  array('Gene' => 'IFNL4','Protein ID' => '2Z62','Start' => '97','end' => '111','Length' => '14','Peptide' => 'AYQSLSHLSTLILTG'),
+  array('Gene' => 'TRBJ2-7','Protein ID' => '','Start' => '2','end' => '14','Length' => '13','Peptide' => 'GANVLTFGAGSRL'),
+  array('Gene' => 'TRBJ2-7','Protein ID' => '','Start' => '2','end' => '16','Length' => '15','Peptide' => 'GANVLTFGAGSRLTV'),
+  array('Gene' => 'TRBJ2-7','Protein ID' => '','Start' => '3','end' => '15','Length' => '13','Peptide' => 'ANVLTFGAGSRLT'),
+  array('Gene' => 'TRBJ2-7','Protein ID' => '','Start' => '3','end' => '16','Length' => '14','Peptide' => 'ANVLTFGAGSRLTV'),
+  array('Gene' => 'TRBJ2-7','Protein ID' => '','Start' => '2','end' => '14','Length' => '13','Peptide' => 'YEQYFGPGTRLTV')
+);
